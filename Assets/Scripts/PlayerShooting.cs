a7085fd0ddaf9fdefd7e6aa7cf5382b564c8328b
Mert Bezirgan <bezirganmert@gmail.com>
2022-06-09 14:15:53 +0300

    added rocket fire

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+  public GameObject bulletPrefab;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Mouse0))
+    {
+      shoot();
+    }
+  }
+
+  void shoot()
+  {
+    GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+  }
+}
